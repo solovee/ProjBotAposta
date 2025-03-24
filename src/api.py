49e934b0177d286c51f87da7875a639fbbe09906
Leonardo Solovijovas Santos <leo.solovijovas@gmail.com>
2025-03-24 14:10:39 -0300

    consertando upcoming

--- src/api.py
+++ src/api.py
@@ -1,6 +1,7 @@
 import requests
-from typing import Dict, Any
+from typing import Dict, Any, List
 from datetime import datetime
+from math import ceil
 
 # Obter a data atual e formatar como YYYYMMDD
 data_atual = datetime.now().strftime('%Y%m%d')
@@ -10,24 +11,25 @@ class BetsAPIClient:
     def __init__(self, api_key: str):
         self.api_key = api_key
         self.base_url = 'https://api.b365api.com/v1/bet365/'
-        self.base_url_betsapi = 'https://api.b365api.com/v1/'
         self.base_url_v3 = 'https://api.b365api.com/v3/bet365/'
+        self._leagues_ids = [10048705, 10047781]
 
-    def getHistory(self, event_id: int = 1, qty: int = 10) -> Dict[str, Any]:
-        """
-        pega o hisórico de partidas
-        """
-        url = f'{self.base_url_betsapi}event/history'
 
-        params = {
-            'token': self.api_key,
-            'event_id' : event_id
-        }
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception(f"Erro ao obter dados da API: {response.status_code}")
+    @property
+    def leagues_ids(self):
+        return self._leagues_ids
+
+
+    def getUpcoming(self, sport_id: int = 1, leagues: List[Any] = [], day: str = data_atual) -> List[Any]:
+        results = []
+        for league in leagues:
+            pages = self.pages(league_id=league)
+            for page in range(1, pages+1):
+                res = self.get_fifa_matches(league_id = league, page = page)
+                results.extend(res)
+        return results
+
+        
 
     def get_inplay_games(self, sport_id: int = 1, league_id: int = 10048705) -> Dict[str, Any]:
 
@@ -44,9 +46,28 @@ class BetsAPIClient:
         else:
             raise Exception(f"Erro ao obter dados da API: {response.status_code}")
         
+    
+    def pages(self, sport_id: int = 1,league_id: int = 10048705, day: str = data_atual) -> int:
+        url = f'{self.base_url}upcoming'
+        params = {
+            'token': self.api_key,
+            'sport_id': sport_id,
+            'league_id': league_id,
+            'day': day,
+        }
+        response = requests.get(url, params=params)
+        resp = response.json()
+        
+        res = ceil(resp['pager']['total'] / resp['pager']['per_page'])
 
+        if response.status_code == 200:
+            return res
+        else:
+            raise Exception(f"Erro ao obter dados da API: {response.status_code}")
+        
+    
 
-    def get_fifa_matches(self, sport_id: int = 1,league_id: int = 10048705, day: str = data_atual) -> Dict[str, Any]:
+    def get_fifa_matches(self, sport_id: int = 1,league_id: int = 10048705, day: str = data_atual, page: int = 1) -> Dict[str, Any]:
         """
         pega jogos de FIFA 8 e 12 minutos da BetsAPI.
         """
@@ -55,21 +76,34 @@ class BetsAPIClient:
             'token': self.api_key,
             'sport_id': sport_id,
             'league_id': league_id,
-            'day': day
-            #precisa de filtragem
+            'day': day,
+            'page': page
+            
         }
         response = requests.get(url, params=params)
         resp = response.json()
         
         res = [x['id'] for x in resp['results'] if (x.get('ss') is None) or x.get('time_status') == 0]
 
-
         if response.status_code == 200:
             return res
         else:
             raise Exception(f"Erro ao obter dados da API: {response.status_code}")
         
-    def getEvent(self, event_id: int = 1):
+    def getInplayEvents(self, sport_id: int = 1, league_id: int = 10048705) -> Dict[str,Any]:
+        url = f'{self.base_url}inplay_filter'
+        params = {
+            'token': self.api_key,
+            'sport_id': sport_id,
+            'league_id': league_id,
+        }
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Erro ao obter odds da API: {response.status_code}")
+        
+    def getEvent(self, event_id: int = 1) -> Dict[str, Any]:
 
         url = f'{self.base_url}result'
 
@@ -97,25 +131,5 @@ class BetsAPIClient:
         else:
             raise Exception(f"Erro ao obter odds da API: {response.status_code}")
     
-    def resultados(self, event_id: int=1):
-        url = f'{self.base_url}result'
+    
 
-        params = {
-            'event_id': event_id,
-            'token': self.api_key
-        }
-        
-
-        '''
-        ligas_esoccer = [liga for liga in ligas['results'] if 'Esoccer' in liga['name']]
-eventos_esoccer = []
-
-for liga in ligas_esoccer:
-    params = {
-        'token': api_key,
-        'league_id': liga['id']
-    }
-    response = requests.get('https://api.betsapi.com/v1/events/upcoming', params=params)
-    eventos = response.json()
-    eventos_esoccer.extend(eventos['results'])
-'''
--- src/testes.py
+++ src/testes.py
@@ -1,21 +1,31 @@
 import requests
 from api import BetsAPIClient
+from dotenv import load_dotenv
+import os
 #'10048705', 'Esoccer GT Leagues - 12 mins play' ;'10047781', 'Esoccer Battle - 8 mins play'
 
 #testar pegar evento 171732570 mais tarde   171790606
+load_dotenv()
 
-
-api = "217012-1ic4weHvqmtEZ0"
+api = os.getenv("API_KEY")
 
 
 apiclient = BetsAPIClient(api_key=api)
 
+ids = apiclient.getUpcoming(leagues=apiclient.leagues_ids)
+print(ids)
 
 
-res = apiclient.get_fifa_matches()
+
+'''
+res = requests.get("https://api.b365api.com/v1/bet365/inplay_filter?sport_id=1&league_id=10047781 &token=217012-1ic4weHvqmtEZ0").json()
+print(res)
+'''
 
 
 
+
+'''
 for id in res:
     odds = apiclient.get_odds(FI=id)['results'][0]['goals']['sp']['goals_over_under']
     print(odds)
@@ -29,7 +39,7 @@ for id in res1:
     odds = apiclient.get_odds(FI=id)['results'][0]['goals']['sp']['goals_over_under']
     print(odds)
 
-
+'''
 
 
 '''
