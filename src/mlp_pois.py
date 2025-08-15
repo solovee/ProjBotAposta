3523188cb79a918c226082e689543d8e648710b6
Leonardo Solovijovas Santos <leo.solovijovas@gmail.com>
2025-08-14 21:37:49 -0300

    arrumando requirements

--- src/mlp_pois.py
+++ src/mlp_pois.py
@@ -1186,10 +1186,10 @@ def treino():
     model_regression_2 = tf.keras.Sequential([
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.BatchNormalization(), 
-        tf.keras.layers.Dropout(0.05),
+        tf.keras.layers.Dropout(0.3),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.BatchNormalization(), 
-        tf.keras.layers.Dropout(0.05),
+        tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(32, activation='relu'),
         tf.keras.layers.Dense(1, activation='linear')
     ])
